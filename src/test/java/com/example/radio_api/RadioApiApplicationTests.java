0f533eaf2d7c6e05e6b31cc2f49556aaacf9360f
ademidek <ademidek@gmail.com>
2025-08-07 22:21:02 -0400

    initial commit

---
+++ src/test/java/com/example/radio_api/RadioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.radio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
