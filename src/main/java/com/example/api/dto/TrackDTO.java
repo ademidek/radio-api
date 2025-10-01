a43f1a2b7afd7ca53413d4705eccc28f2d70108d
ademidek <ademidek@gmail.com>
2025-10-01 19:53:20 -0400

    started track DTO

--- src/main/java/com/example/api/dto/TrackDTO.java
+++ src/main/java/com/example/api/dto/TrackDTO.java
@@ -1,5 +1,29 @@
-package com.example.radio_api.dto;
+package com.example.api.dto;
 
 public class TrackDTO {
+
+    @NotBlank
+    @Size(max = 100)
+    private String trackName;
+    
+    @NotBlank
+    @Size(max = 100)
+    private String trackArtist;
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getTrackArtist() {
+        return trackArtist;
+    }
+
+    public void setTrackArtist(String trackArtist) {
+        this.trackArtist = trackArtist;
+    }
     
 }
--- src/main/java/com/example/api/service/TrackService.java
+++ src/main/java/com/example/api/service/TrackService.java
@@ -2,4 +2,11 @@ package com.example.api.service;
 
 public class TrackService {
 
+    @Autowired
+    private TrackRepository trackRepository;
+
+    public List<Track> getAllTracks() {
+        return trackRepository.findAll();
+    }
+
 }
