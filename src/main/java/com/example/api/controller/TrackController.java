1b0fdc0d574fa342b58ccd7b60efc93adfd57663
ademidek <ademidek@gmail.com>
2025-10-05 14:11:35 -0400

    added track route

--- src/main/java/com/example/api/controller/TrackController.java
+++ src/main/java/com/example/api/controller/TrackController.java
@@ -13,13 +13,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.net.URL;
 import java.time.Duration;
+import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/tracks")
 public class TrackController {
     @GetMapping("/")
     public String index(){
-        return "Hello World!";
+        return "Welcome to AK(tion) Radio.";
     }
 
     private final TrackRepository trackRepository;
@@ -30,13 +32,28 @@ public class TrackController {
         this.s3AudioService = s3AudioService;
     }
 
+    @GetMapping("/tracks")
+    public String showTracks(){
+        return "Showing all tracks";
+    }
+
     @GetMapping("/tracks/{id}")
     public Map<String, Object> generatePresignedUrl(@PathVariable Integer id,
                                                @RequestParam(defaultValue = "10") int minutes) {
         Track track = trackRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Track " + id + " not found"));
         URL url = s3AudioService.generatePresignedUrl(track.getS3Key());
         return Map.of("url", url.toString());
+    }
 
-}
+    /*@GetMapping
+    public List<Map<String,Object>> list() {
+    return trackRepository.findAll().stream()
+      .map(t -> Map.of(
+        "id", t.getTrackId(),
+        "name", t.getTrackName(),
+        "artist", t.getTrackArtist(),
+        "s3Key", t.getS3Key()))
+      .toList();
+  }*/
 }
 
